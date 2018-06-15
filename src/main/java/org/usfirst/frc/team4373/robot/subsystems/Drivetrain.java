5ba7961bdb8a568d3289a8febefd665de8e18402
Sam Gauck <21samuelg@abingtonfriends.net>
2018-06-15 16:13:08 -0400

    Set up Drivetrain skeleton

--- src/main/java/org/usfirst/frc/team4373/robot/subsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc/team4373/robot/subsystems/Drivetrain.java
@@ -1,4 +1,32 @@
 package org.usfirst.frc.team4373.robot.subsystems;
 
-public class Drivetrain {
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * A programmatic representation of the robot's drivetrain
+ *
+ * @author Samasaur
+ */
+public class Drivetrain extends Subsystem {
+
+    //declare variables
+
+    private static Drivetrain instance;
+
+    public static Drivetrain getInstance() {
+        return instance == null ? instance = new Drivetrain() : instance;
+    }
+
+    private Drivetrain() {
+        //initialize variables
+    }
+
+    //setting methods go here
+
+    //getting methods go here
+
+    @Override
+    protected void initDefaultCommand() {
+//        setDefaultCommand(new BalancingCommand()); //it's a balancing act
+    }
 }
