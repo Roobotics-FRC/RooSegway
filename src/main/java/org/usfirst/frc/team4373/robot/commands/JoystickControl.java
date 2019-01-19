8266a080cd6cc71f96f62db57139079a528c7076
aaplmath <aaplmath@users.noreply.github.com>
2019-01-19 15:11:02 -0500

    Add code to log positions

--- src/main/java/org/usfirst/frc/team4373/robot/commands/JoystickControl.java
+++ src/main/java/org/usfirst/frc/team4373/robot/commands/JoystickControl.java
@@ -1,9 +1,14 @@
 package org.usfirst.frc.team4373.robot.commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team4373.robot.OI;
 import org.usfirst.frc.team4373.robot.subsystems.Drivetrain;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.HashMap;
 
 /**
  * Provides joystick-based control of the apparatus with a "ramping" input mechanism that caps
@@ -16,7 +21,11 @@ public class JoystickControl extends Command {
 
     private Drivetrain drivetrain;
 
+    private HashMap<Long, Integer> logs;
+    private boolean logged = false;
+
     public JoystickControl() {
+        this.logs = new HashMap();
         requires(this.drivetrain = Drivetrain.getInstance());
     }
 
@@ -37,13 +46,18 @@ public class JoystickControl extends Command {
         double newRight = y + z;
         double newLeft = y - z;
 
-        double rightDiff = newRight - curRight;
-        rightDiff = Math.abs(rightDiff) > 0.01 ? Math.copySign(0.01, rightDiff) : rightDiff;
-        double leftDiff = newLeft - curLeft;
-        leftDiff = Math.abs(leftDiff) > 0.01 ? Math.copySign(0.01, leftDiff) : leftDiff;
-
-        drivetrain.setRight(curRight + rightDiff);
-        drivetrain.setLeft(curLeft + leftDiff);
+        // double rightDiff = newRight - curRight;
+        // rightDiff = Math.abs(rightDiff) > 0.01 ? Math.copySign(0.01, rightDiff) : rightDiff;
+        // double leftDiff = newLeft - curLeft;
+        // leftDiff = Math.abs(leftDiff) > 0.01 ? Math.copySign(0.01, leftDiff) : leftDiff;
+        //
+        // drivetrain.setRight(curRight + rightDiff);
+        // drivetrain.setLeft(curLeft + leftDiff);
+        this.drivetrain.setBoth(y);
+        if (OI.getOI().getDriveJoystick().getRawButton(7)) {
+            this.drivetrain.setRight(y + z);
+            this.drivetrain.setLeft(y - z);
+        }
 
         // Shifting debug
         if (OI.getOI().getDriveJoystick().getRawButton(6)) {
@@ -52,13 +66,30 @@ public class JoystickControl extends Command {
             this.drivetrain.shift(Drivetrain.Gear.LOW);
         }
 
+        this.logs.put(System.currentTimeMillis(), this.drivetrain.getRightPosition());
+
         SmartDashboard.putNumber("Right Velocity", this.drivetrain.getRightVelocity());
         SmartDashboard.putNumber("Left Velocity", this.drivetrain.getLeftVelocity());
+
+        // Write out on press of button 10
+        if (OI.getOI().getDriveJoystick().getRawButton(10) && !logged) {
+            System.out.println("********************************");
+            logged = true;
+            StringBuilder sb = new StringBuilder();
+            logs.forEach((Long time, Integer log) -> {
+                sb.append(time.toString());
+                sb.append(",");
+                sb.append(log.toString());
+                sb.append("\n");
+            });
+            System.out.println(sb.toString());
+            System.out.println("********************************");
+        }
     }
 
     @Override
     public void interrupted() {
-        this.drivetrain.setBoth(0);
+        this.end();
     }
 
     @Override
